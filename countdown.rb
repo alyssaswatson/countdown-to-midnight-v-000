d3b5303e8d266b00ab1f386fe05cea76b7dcaaf2
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-13 14:34:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(timer)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(timer)
+  while timer > 0
+    puts "#{timer} SECOND(S)!"
+    timer -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
+end
